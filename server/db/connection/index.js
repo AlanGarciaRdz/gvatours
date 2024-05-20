36a6ad84e84b124d688b9a3004764a4da6b86341
Alan garcia <agarcia@brightcove.com>
2024-05-20 12:01:33 -0600

    connection

--- server/db/connection/index.js
+++ server/db/connection/index.js
@@ -1,26 +1,36 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 var pool;
 
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === "development"){
-    pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'postgres',
-        password: 'password',
-        port: 5432,
-    })
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "postgres",
+    password: "password",
+    port: 5432,
+  });
 }
 
-if (process.env.NODE_ENV === "production"){ 
-    pool = new Pool({
-        user: 'postgres',
-        host: 'database-gva.ctdlpwrrgw3t.us-east-2.rds.amazonaws.com', //ohio
-        database: 'postgres',
-        password: 'abcd1234!',
-        port: 5432,
-    })
+// if (process.env.NODE_ENV === "production"){
+//     pool = new Pool({
+//         user: 'postgres',
+//         host: 'database-gva.ctdlpwrrgw3t.us-east-2.rds.amazonaws.com', //ohio
+//         database: 'postgres',
+//         password: 'abcd1234!',
+//         port: 5432,
+//     })
+// }
+
+if (process.env.NODE_ENV === "production") {
+  pool = new Pool({
+    user: "postgres",
+    host: "ec2-3-90-18-216.compute-1.amazonaws.com", //ec2
+    database: "postgres",
+    password: "abcd1234!",
+    port: 5432,
+  });
 }
 //psql -U postgres -h database-gva.ctdlpwrrgw3t.us-east-2.rds.amazonaws.com -p 5432 postgres
 
-exports.pool =  pool;
+exports.pool = pool;
